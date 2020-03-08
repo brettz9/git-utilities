211d393bc0e6d86c242d6ac38046113879bac311
Brett Zamir <brettz9@yahoo.com>
2020-03-08 10:33:20 +0800

    - Testing: Ensure `open` will be as a URL

--- test/openGitURL.js
+++ test/openGitURL.js
@@ -4,8 +4,14 @@ const proxyquire = require('proxyquire');
 
 let openURL = null;
 const {openGitURL} = proxyquire('../src/index.js', {
-  open (url) {
+  // Todo: Stub `dialog` too to catch its error reporting
+  open (url, config) {
     openURL = url;
+    if (!config || config.url !== true) {
+      // We can refactor here if we need to call `open` in a mode
+      //   other than for URLs
+      throw new Error('Will not open as URL.');
+    }
   }
 });
 
